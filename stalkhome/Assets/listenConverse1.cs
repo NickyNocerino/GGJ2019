dbf9bfb3e1976ca3827c672ee815a08d1eb9d525
NickyNocerino <nocerint@miamioh.edu>
2019-01-26 15:06:46 -0500

    image zoom window
    click for better view of signs

---
+++ stalkhome/Assets/listenConverse1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class listenConverse1 : MonoBehaviour
+{
+    public GameObject dialogBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseDown()
+    {
+        dialogBox.SetActive(true);
+        dialogBox.GetComponentInChildren<Text>().text = "yo dawg whats crackalackin";
+
+    }
+}
--- stalkhome/Assets/secretTexts.cs
+++ stalkhome/Assets/secretTexts.cs
@@ -8,6 +8,10 @@ public class secretTexts : MonoBehaviour
     public string secretText;
     public string trans = "";
     public GameObject transText;
+    public bool sign;
+    public GameObject signBox;
+    public GameObject signSign;
+    public Sprite zoomSign;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,9 +31,9 @@ public class secretTexts : MonoBehaviour
         {
             if (globalVars.masterCipher.ContainsKey(char.ToLower(c).ToString()))
             {
-                if (globalVars.masterCipher[char.ToLower(c).ToString()] == "-")
+                if (globalVars.masterCipher[char.ToLower(c).ToString()] == "-" || globalVars.masterCipher[char.ToLower(c).ToString()] == "")
                 {
-                    trans += "*";
+                    trans += "?";
                 }
                 else
                 {
@@ -44,16 +48,98 @@ public class secretTexts : MonoBehaviour
             }
             else
             {
-                trans += "*";
+                trans += "?";
             }
         }
         print(trans.ToString());
         transText.SetActive(true);
         transText.transform.position = Input.mousePosition;
         transText.GetComponentInChildren<Text>().text = trans;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (sign)
+            {
+                signBox.SetActive(true);
+                Image im = signSign.GetComponent<Image>();
+                im.sprite = zoomSign;
+                signBox.GetComponentInChildren<secretTexts>().secretText = secretText;
+            }
+        }
     }
     void OnMouseExit()
     {
         transText.SetActive(false);
+    }
+    public void OnPointerEnter(GameObject tran)
+    {
+       print("found it");
+       tran.SetActive(true);
+       trans = "";
+       secretText = this.GetComponent<Text>().text;
+       foreach (char c in secretText)
+       {
+           if (globalVars.masterCipher.ContainsKey(char.ToLower(c).ToString()))
+           {
+               if (globalVars.masterCipher[char.ToLower(c).ToString()] == "-" || globalVars.masterCipher[char.ToLower(c).ToString()] == "")
+               {
+                   trans += "?";
+               }
+               else
+               {
+                  // Debug.Log(globalVars.masterCipher[char.ToLower(c).ToString()]);
+                   trans += globalVars.masterCipher[char.ToLower(c).ToString()];
+               }
+           }
+           else if (c.ToString() == " ")
+           {
+                trans += " ";
+           }
+           else
+           {
+               trans += "?";
+           }
+       }
+      // print(trans.ToString());
+      // tran.transform.position = Input.mousePosition;
+       tran.GetComponentInChildren<Text>().text = trans;
+    }
+    public void OnPointerExit(GameObject tran)
+    {
+      //  print("lost it");
+        tran.SetActive(false);
+
+    }
+    public void OnPointerEnterImage(GameObject tran)
+    {
+        print("found it");
+        tran.SetActive(true);
+        trans = "";
+        //secretText = this.GetComponent<Text>().text;
+        foreach (char c in secretText)
+        {
+            if (globalVars.masterCipher.ContainsKey(char.ToLower(c).ToString()))
+            {
+                if (globalVars.masterCipher[char.ToLower(c).ToString()] == "-" || globalVars.masterCipher[char.ToLower(c).ToString()] == "")
+                {
+                    trans += "?";
+                }
+                else
+                {
+                    // Debug.Log(globalVars.masterCipher[char.ToLower(c).ToString()]);
+                    trans += globalVars.masterCipher[char.ToLower(c).ToString()];
+                }
+            }
+            else if (c.ToString() == " ")
+            {
+                trans += " ";
+            }
+            else
+            {
+                trans += "?";
+            }
+        }
+        // print(trans.ToString());
+        // tran.transform.position = Input.mousePosition;
+        tran.GetComponentInChildren<Text>().text = trans;
     }
 }
