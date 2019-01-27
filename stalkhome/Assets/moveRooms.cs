69d9ab66ac87307a8d38026e441c9d2f210178c6
NickyNocerino <nocerint@miamioh.edu>
2019-01-27 01:08:53 -0500

    just a whole bunch of stuff
    added a lot

---
+++ stalkhome/Assets/moveRooms.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moveRooms : MonoBehaviour
+{
+    public GameObject newCam;
+    public GameObject cam;
+    public string roomName;
+    public GameObject transText;
+    private string trans;
+    public GameObject sign;
+    public GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseOver()
+    {
+        trans = "";
+        foreach (char c in roomName)
+        {
+            if (globalVars.masterCipher.ContainsKey(char.ToLower(c).ToString()))
+            {
+                if (globalVars.masterCipher[char.ToLower(c).ToString()] == "-" || globalVars.masterCipher[char.ToLower(c).ToString()] == "")
+                {
+                    trans += "?";
+                }
+                else
+                {
+
+                    Debug.Log(globalVars.masterCipher[char.ToLower(c).ToString()]);
+                    trans += globalVars.masterCipher[char.ToLower(c).ToString()];
+                }
+            }
+            else if (c.ToString() == " ")
+            {
+                trans += " ";
+            }
+            else
+            {
+                trans += "?";
+            }
+        }
+        print(trans.ToString());
+        transText.SetActive(true);
+        transText.transform.position = Input.mousePosition;
+        transText.GetComponentInChildren<Text>().text = trans;
+        if (Input.GetMouseButtonDown(0))
+        {
+            cam.GetComponent<Transform>().position = newCam.GetComponent<Transform>().position;
+            sign.SetActive(false);
+            text.SetActive(false);
+        }
+    }
+    void OnMouseExit()
+    {
+        transText.SetActive(false);
+    }
+}
--- stalkhome/Assets/secretTexts.cs
+++ stalkhome/Assets/secretTexts.cs
@@ -8,10 +8,13 @@ public class secretTexts : MonoBehaviour
     public string secretText;
     public string trans = "";
     public GameObject transText;
+    public GameObject tran;
     public bool sign;
     public GameObject signBox;
     public GameObject signSign;
     public Sprite zoomSign;
+    public bool speak;
+    public GameObject speakBox;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +25,36 @@ public class secretTexts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //print("found it");
+        tran.SetActive(true);
+        trans = "";
+        //secretText = this.GetComponent<Text>().text;
+        foreach (char c in secretText)
+        {
+            if (globalVars.masterCipher.ContainsKey(char.ToLower(c).ToString()))
+            {
+                if (globalVars.masterCipher[char.ToLower(c).ToString()] == "-" || globalVars.masterCipher[char.ToLower(c).ToString()] == "")
+                {
+                    trans += "?";
+                }
+                else
+                {
+                    // Debug.Log(globalVars.masterCipher[char.ToLower(c).ToString()]);
+                    trans += globalVars.masterCipher[char.ToLower(c).ToString()];
+                }
+            }
+            else if (c.ToString() == " ")
+            {
+                trans += " ";
+            }
+            else
+            {
+                trans += "?";
+            }
+        }
+        // print(trans.ToString());
+        // tran.transform.position = Input.mousePosition;
+        tran.GetComponent<Text>().text = trans;
     }
     void OnMouseOver()
     {
@@ -59,10 +91,17 @@ public class secretTexts : MonoBehaviour
         {
             if (sign)
             {
+                speakBox.SetActive(false);
                 signBox.SetActive(true);
                 Image im = signSign.GetComponent<Image>();
                 im.sprite = zoomSign;
                 signBox.GetComponentInChildren<secretTexts>().secretText = secretText;
+            }
+            if(speak)
+            {
+                signBox.SetActive(false);
+                speakBox.SetActive(true);
+                speakBox.GetComponentInChildren<secretTexts>().secretText = secretText;
             }
         }
     }
@@ -72,7 +111,7 @@ public class secretTexts : MonoBehaviour
     }
     public void OnPointerEnter(GameObject tran)
     {
-       print("found it");
+       //print("found it");
        tran.SetActive(true);
        trans = "";
        secretText = this.GetComponent<Text>().text;
@@ -111,7 +150,7 @@ public class secretTexts : MonoBehaviour
     }
     public void OnPointerEnterImage(GameObject tran)
     {
-        print("found it");
+        //print("found it");
         tran.SetActive(true);
         trans = "";
         //secretText = this.GetComponent<Text>().text;
